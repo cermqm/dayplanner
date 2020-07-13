28ab34d9421658db68862418e356fd25fed80c9c
Michael Mink <mmink@michaelamink.com>
2020-07-12 22:32:45 -0500

    initial upload

---
+++ dayplanner.js
@@ -0,0 +1,116 @@
+var test = true;
+
+// function currentday() {
+
+var tdate = new Date();
+if (test) console.log(tdate);
+var strArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+var d = tdate.getDate();
+var m = strArray[tdate.getMonth()];
+var y = tdate.getFullYear();
+var h = tdate.getHours();
+// var m = tdate.getMinutes();
+
+if (test) console.log(h);
+currdate = '' + (d <= 9 ? '0' + d : d) + '-' + m + '-' + y;
+if (test) console.log(currdate);
+
+var todo = ["", "", "", "", "", "", "", "", ""];
+
+
+// }
+
+function writeJumbo() {
+
+    var jumbop1 = $("<p>").attr({ id: "jumbop" }).addClass("lead text-center").text("");
+    var jumbohtag = $("<h1>").attr({ id: "jumboh1" }).addClass("display-4 text-center").text("Day Planner");
+    var jumbohtag2 = $("<h1>").attr({ id: "jumboh12" }).addClass("display-4 text-center ").text(currdate);
+    var jumboContainer = $("<div>").attr({ id: "jumboContainer" }).addClass("container");
+    jumboContainer.append(jumbohtag, jumbohtag2, jumbop1);
+    var jumbo1 = $("<div>").attr({ id: "jumbo1" }).addClass("jumbotron jumbotron-fluid border border-primary rounded-lg bg-primary");
+    jumbo1.append(jumboContainer);
+    $("#jumbodiv").append(jumbo1);
+
+}
+
+function writePage() {
+    var businessHours = ["0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700"];
+
+    for (let i = 0; i < businessHours.length; i++) {
+        if (test) console.log("in for loop");
+        // Adding work day times
+        var plannerTimes = $("<a>").text(businessHours[i]).attr({ id: "hours" + i, float: "left" }).addClass("border border-secondary rounded-lg hours");
+        // Adding input box
+        var inputid = ("input" + i);
+        if (test) console.log("i = " + i);
+        if (test) console.log("todo[i] = " + todo[i]);
+        var plannerInput = $("<input>").attr({ id: inputid, value: todo[i], float: "left", size: "40" }).addClass("border border-secondary rounded-lg input");
+        // Adding save button
+        var plannerSave = $("<button>").attr({ id: "save" + i, value: i }).addClass("border border-secondary rounded-lg button fa fa-floppy-o");
+        // Creating a div container for these 3 items...
+        var container = $("<div>").attr({ id: "container" + i, style: "font-size: 2vw" });
+        container.append(plannerTimes, plannerInput, plannerSave);
+        $("#startdiv").append(container);
+        updateRowColor(businessHours[i], inputid);
+    }
+
+}
+
+// init function to pull highscores if there are any and put into highScorearray...
+function init(parameter) {
+    if (test) console.log("todo array in init = " + todo)
+    var todosaved = JSON.parse(localStorage.getItem("todos-" + currdate));
+    if (todosaved !== null) {
+        todo = todosaved;
+        if (test) console.log("todo array in init after localstorage read = " + todo)
+    }
+}
+
+function saveRow(index) {
+    var inputText = $("#input" + index).val();
+    if (test) console.log("inputText = " + inputText);
+    if (test) console.log("todo array = " + todo);
+    if (test) console.log("index = " + index);
+    todo.splice(index, 1, inputText);
+    if (test) console.log("todo array = " + todo);
+    localStorage.setItem("todos-" + currdate, JSON.stringify(todo));
+
+}
+
+// function to update row color
+function updateRowColor(hour, inputid) {
+
+    if (test) console.log("rowColor ", h, hour, inputid);
+
+    var time2 = String(h) + "00";
+    if (test) console.log("time2 = " + time2);
+
+    if (hour < time2) {
+        // update inputid.css('')
+        if (test) { console.log("lessThan"); }
+        $(`#${inputid}`).css("background-color", "lightgrey");
+        //add disabled attribut on element
+    } else if (hour > time2) {
+        if (test) console.log("greaterthan");
+        $(`#${inputid}`).css("background-color", "lightgreen")
+    } else {
+        if (test) console.log("equal");
+        $(`#${inputid}`).css("background-color", "tomato")
+    }
+};
+
+init();
+writeJumbo();
+writePage();
+
+//location reload and set a timer
+//or set an interval
+
+// listener for save button
+$(".button").on("click", function() {
+    // if (test) alert("button clicked");
+    var valueofbtn = this.value;
+    if (test) console.log("button clicked - value - " + this.value)
+    if (test) console.log("valueofbtn = " + valueofbtn);
+    saveRow(valueofbtn);
+});
